f4c21c6d2a18201d6005a988a08b64cdf50293b6
Shee Jia Wei <130957474+SheeJiaWei0531@users.noreply.github.com>
2023-06-20 11:06:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let taxiFareApiUrl = 'https://jw-oc47rw55ea-ew.a.run.app/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
-  taxiFareApiUrl = 'https://jw-oc47rw55ea-ew.a.run.app/predict';
+  taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
